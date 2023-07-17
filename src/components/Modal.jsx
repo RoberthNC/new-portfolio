70ed08fcb091e12f22cb04a4b5858595de25a1d7
Roberto Núñez <robertonc1599@gmail.com>
2023-07-17 13:56:51 -0500

    Links 95%

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,21 @@
+import useApp from "../hooks/useApp"
+
 const Modal = () => {
-  return (
-    <div className="absolute z-10 bg-[#0a192f] h-screen w-full flex flex-col items-center justify-center gap-5">
-        <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">About me</a>
-        <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">Skills</a>
-        <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">Projects</a>
-        <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">Contact me</a>
-    </div>
-  )
+
+    const { setIsVisible } = useApp()
+
+    const hideModal = () => {
+        setIsVisible( v => setIsVisible(!v) )
+    }
+
+    return (
+        <div className="absolute z-10 bg-[#0a192f] h-screen w-full flex flex-col items-center justify-center gap-5">
+            <a href="#" className="text-white py-[8px] hover:border-b-[1px] hover:border-white text-lg" onClick={hideModal}>About me</a>
+            <a href="#" className="text-white py-[8px] hover:border-b-[1px] hover:border-white text-lg" onClick={hideModal}>Skills</a>
+            <a href="#" className="text-white py-[8px] hover:border-b-[1px] hover:border-white text-lg" onClick={hideModal}>Projects</a>
+            <a href="#" className="text-white py-[8px] hover:border-b-[1px] hover:border-white text-lg" onClick={hideModal}>Contact me</a>
+        </div>
+    )
 }
 
 export default Modal
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,11 @@ const Navbar = () => {
 
     const handleModal = (e) => {
         e.preventDefault();
-
         setIsVisible( v => setIsVisible(!v) )
     }
 
     return (
-        <div className="bg-[#0a192f] p-3 sm:px-5 flex items-center justify-between fixed left-0 top-0 right-0">
+        <div className="bg-[#0a192f] py-3 px-5 flex items-center justify-between fixed left-0 top-0 right-0">
             <img src={ Logo } alt="My personal logo" />
             <nav className="hidden sm:flex sm:justify-center sm:gap-5">
                 <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">About me</a>
@@ -21,8 +20,8 @@ const Navbar = () => {
                 <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">Projects</a>
                 <a href="#" className="text-white text-base py-[8px] hover:border-b-[1px] hover:border-white">Contact me</a>
             </nav>
-            <button onClick={handleModal}>
-                <img className="cursor-pointer block sm:hidden" src={ MenuBars } alt="Menú bars" />
+            <button onClick={handleModal} className="sm:hidden">
+                <img className="cursor-pointer block" src={ MenuBars } alt="Menú bars" />
             </button>
         </div>
   )
