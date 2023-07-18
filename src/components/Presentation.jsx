6f612909f01c2c2b2c4fff7426bc4dc7d6988207
Roberto Núñez <robertonc1599@gmail.com>
2023-07-17 23:12:28 -0500

    Some changes on the color

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -7,7 +7,7 @@ const Presentation = () => {
         <p className="text-[#01be96] text-base">Hi! my name is</p>
         <p className="text-blue-100 text-3xl md:text-5xl font-bold">Roberto Núñez</p>
         <p className="text-gray-500 text-3xl md:text-5xl font-bold">I'm a Full-Stack Developer</p>
-        <p className="text-gray-500 text-base text-justify">I consider myself as a self-taught person who likes to learn and solve challenges. I like teamwork, I'm one of the people who believe that a person can do many things but a team can reach limits unimaginable.</p>
+        <p className="text-blue-100 text-base text-justify">I consider myself as a self-taught person who likes to learn and solve challenges. I like teamwork, I'm one of the people who believe that a person can do many things but a team can reach limits unimaginable.</p>
 
         <a href={Pdf} download={"Roberto Núñez - CV"} className="inline-block border-[2px] px-5 py-2 mt-3 hover:border-[#01be96] hover:bg-[#01be96] transition-colors duration-300 w-44">
             <div className="flex gap-[5px] items-center">
