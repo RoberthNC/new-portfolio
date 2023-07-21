ee85013a7e79d162311e4e3b731031274d4dcf9b
Roberto Núñez <robertonc1599@gmail.com>
2023-07-20 23:03:02 -0500

    Sección de proyectos terminada

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,10 @@ const Project = ({name, img, repository}) => {
 
   return (
     <div style={styles} className="text-center h-28 sm:h-40 rounded-md">
-      <p>{ name }</p>
+      <div className="bg-black h-full bg-opacity-30 flex flex-col items-center justify-center gap-3">
+        <p className="bg-opacity-100 text-blue-100 text-center text-sm sm:text-base">{ name }</p>
+        <a href={repository} target="_blank" className="bg-opacity-100 text-blue-100 border-[2px] p-1 hover:border-[#01be96] hover:bg-[#01be96] transition-colors duration-300 text-sm sm:text-base">View Code</a>
+      </div>
     </div>
   )
 }
